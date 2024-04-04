06138ebe523386f081fad32cc32f9bbad9c0b172
Lucas C. Bresolino <49660780+LucasChaosMind@users.noreply.github.com>
2024-04-03 21:05:11 -0300

    Menu bar selection in console application in C#
    First Home Work

---
+++ MenuConsoleApp/SistemaConsole/Menu.cs
@@ -0,0 +1,142 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SistemaConsole
+{
+
+    internal class MenuItem
+    {
+        public string Rotulo { get; set; }
+        public int Col { get; set; }
+        public int Lin { get; set; }
+        public ConsoleColor ItemForeground { get; set; }
+        public ConsoleColor ItemBackground { get; set; }
+        public ConsoleColor SelectorForeground { get; set; }
+        public ConsoleColor SelectorBackground { get; set; }
+
+        public MenuItem()
+        {
+            ItemForeground = ConsoleColor.White;
+            ItemBackground = ConsoleColor.Black;
+            SelectorBackground = ConsoleColor.Blue;
+            SelectorForeground = ConsoleColor.Yellow;
+        }
+
+        public void Show()
+        {
+            Console.BackgroundColor = ItemBackground;
+            Console.ForegroundColor = ItemForeground;
+            Console.SetCursorPosition(Col, Lin);
+            Console.Write(Rotulo);
+            Console.SetCursorPosition(Col, Lin);
+        }
+
+        public void ShowSelector()
+        {
+            Console.BackgroundColor = SelectorBackground;
+            Console.ForegroundColor = SelectorForeground;
+            Console.SetCursorPosition(Col, Lin);
+            Console.Write(Rotulo);
+            Console.SetCursorPosition(Col, Lin);
+        }
+        
+    }
+    internal class Menu
+    {
+        public List<MenuItem> Items { get; set; }
+        public List<MenuItem> BarItems { get; set;}
+        public int PosAtual { get; set; }
+        public string Titulo { get; set; }
+        public bool Centralizado { get; set; }
+        public ConsoleColor TitleForeground { get; set; }
+        public ConsoleColor TitleBackground { get; set; }
+
+        public Menu(string titulo)
+        {
+            Items = new List<MenuItem>();
+            BarItems = new List<MenuItem>();
+            TitleBackground = ConsoleColor.Blue;
+            TitleForeground = ConsoleColor.Yellow;
+            Titulo = titulo;
+        }
+
+        private int Select()
+        {
+            Items[PosAtual].ShowSelector();
+            while (true)
+            {
+                var tecla = Console.ReadKey();
+                Items[PosAtual].Show();
+                switch (tecla.Key)
+                {
+                    case ConsoleKey.Enter: return PosAtual;
+                    case ConsoleKey.Escape: return -1; 
+                    case ConsoleKey.DownArrow:
+                        {
+                            if (++PosAtual == Items.Count) PosAtual = 0;
+                        }
+                        break;
+                    case ConsoleKey.UpArrow:
+                        {
+                            if (--PosAtual < 0) PosAtual = Items.Count - 1;
+                        }
+                        break;
+                }
+                Items[PosAtual].ShowSelector();
+            }
+        }
+
+        public void Show()
+        {
+            int x = 0, y = 0;
+            PosAtual = 0;
+            Console.Clear();
+            if(Items.Count == 0)
+            {
+                throw new ArgumentException("Menu não contém items definidos.");
+            }
+            if (BarItems.Count == 0)
+            {
+                throw new ArgumentException("Menu não contém items definidos.");
+            }
+            var XIndex = 4;
+            foreach (MenuItem m in BarItems)
+            {
+                m.Lin = 1;
+                m.Col = XIndex;
+                m.Show();
+                XIndex += m.Rotulo.Length + 4;
+            }
+            if (Centralizado)
+            {
+                x = (Console.WindowWidth - Titulo.Length) / 2;
+                y = (Console.WindowHeight - Items.Count - 2) / 2;
+            }
+            //mostra titulo
+            Console.BackgroundColor = TitleBackground;
+            Console.ForegroundColor = TitleForeground;
+            Console.SetCursorPosition(x, y);
+            Console.WriteLine(Titulo);
+            Console.WriteLine();
+            y += 2;
+            foreach (MenuItem m in Items)
+            {
+                if(Centralizado)
+                {
+                    x = (Console.WindowWidth - m.Rotulo.Length) / 2;
+                }
+                m.Lin = y++;
+                m.Col = x;
+                m.Show();
+            }
+            Console.CursorVisible = false;
+            var selected = Select();
+            Console.CursorVisible = true;
+        }
+
+        
+    }
+}
---
+++ MenuConsoleApp/SistemaConsole/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SistemaConsole
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Menu menu = new Menu("MENU");
+            menu.Centralizado = true;
+            menu.Items.Add(new MenuItem { Rotulo = "Opcao 1" });
+            menu.Items.Add(new MenuItem { Rotulo = "Opcao 2" });
+            menu.Items.Add(new MenuItem { Rotulo = "Opcao 3" });
+
+            menu.BarItems.Add(new MenuItem { Rotulo = "Opcao 1" });
+            menu.BarItems.Add(new MenuItem { Rotulo = "Opcao 2" });
+            menu.BarItems.Add(new MenuItem { Rotulo = "Opcao 3" });
+
+            menu.Show();
+            Console.ReadKey();  
+       }
+    }
+}
